737991f6a5e533d00b8fdd657234640226983ddd
LuchoMoreno <lucho.moreno@live.com>
2018-02-19 12:25:11 -0300

    **EJERCICIO 5 "WHILE" ACTUALIZADO**

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,11 +1,14 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+var sexo;// = prompt("ingrese f ó m .");
 
 while (sexo!="f" && sexo!="m")
 {
     sexo=prompt("Ingrese nuevamente f o m");
+    if(sexo==null)
+        break;
+
 }
 
 document.getElementById('Sexo').value=sexo;
