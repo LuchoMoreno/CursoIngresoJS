ce77c9af762a3ccfc93e09e9592fe521a071bde6
LuchoMoreno <lucho.moreno@live.com>
2018-02-24 12:43:10 -0300

    Todos los ejercicios de FOR resueltos

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,25 @@
 function Mostrar()
 {
-
-
+    var divisores=0;
+    var ingreso = prompt("Ingrese un numero: ");
+    ingreso = parseInt(ingreso);
+    
+    for(var i = 1; ingreso+1 > i; i++)
+    {
+        while(isNaN(ingreso))
+        {
+            ingreso = prompt("Ingrese un numero:");
+            ingreso = parseInt(ingreso);
+        }
+        
+        if(ingreso%i==0)
+        {
+            console.log(i);
+            divisores++;
+        }
+    
+    }
+    alert("Cantidad de numeros divisores: " +divisores);
 
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,31 @@
 function Mostrar()
 {
-
-
-
+    var contador = 0;
+    var ingreso = prompt("Ingrese un numero: ");
+    ingreso = parseInt(ingreso);
+    
+    for(var i=1 ; i<=ingreso ; i++ )
+    {
+        while(isNaN(ingreso))
+        {
+            ingreso = prompt("Ingrese un numero:");
+            ingreso = parseInt(ingreso);
+        }
+        
+        if(ingreso%i==0)
+        {
+            console.log(i);
+            contador++;
+        }
+    
+    }
+    if (contador==2)
+    {
+        alert("Es un numero primo.")
+    }
+    else if (contador>2)
+    {
+        alert("El numero NO es primo")
+    }
 
 }//FIN DE LA FUNCIÓN
